34ecbde86a3e04983d9a0d99a493cfc57a037572
CyroRycil <ryckebuschcyril@gmail.com>
2026-03-28 16:19:23 +0100

    Fix eslint et next config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    turbopack: {
-      resolveAlias: {
-        '@': '.',
-      },
-    },
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
